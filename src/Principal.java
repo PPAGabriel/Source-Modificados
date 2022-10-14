f7c0a47dacf46e22d9a96a7d5afe7854e5aef2dd
Gabriel Perez <gperezpereira@danielcastelao.org>
2022-10-14 09:05:33 +0200

    Terminada Version 1.0

--- src/Principal.java
+++ src/Principal.java
@@ -1,5 +1,10 @@
+import javax.swing.*;
+
+/**
+ * @version 1.0
+ */
 public class Principal {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        JOptionPane.showMessageDialog(null,"Version 1.0");
     }
 }
