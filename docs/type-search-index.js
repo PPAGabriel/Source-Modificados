cd88de94e0ba61c95bd80c13c694eb351eaea659
Gabriel Perez <gperezpereira@danielcastelao.org>
2022-11-24 08:50:03 +0100

    html del java

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"CuartaClase"},{"p":"<Unnamed>","l":"Principal"},{"p":"<Unnamed>","l":"Secundaria"},{"p":"<Unnamed>","l":"UltimaClase"},{"p":"<Unnamed>","l":"UnaMas"}]
